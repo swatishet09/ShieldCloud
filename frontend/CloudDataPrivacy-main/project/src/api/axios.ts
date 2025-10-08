392e8872d861ccbd1451e0316632033960552b3d
Sneha Shanbhag <159918012+Sneha02Shanbhag@users.noreply.github.com>
2025-10-08 20:05:20 +0530

    Update axios.ts

--- frontend/CloudDataPrivacy-main/project/src/api/axios.ts
+++ frontend/CloudDataPrivacy-main/project/src/api/axios.ts
@@ -1,8 +1,8 @@
 
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
-
+//const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
+const BASE_URL = "https://oq8l79wl3m.execute-api.ap-south-1.amazonaws.com/store";
 export default axios.create({
   baseURL: BASE_URL,
   headers: {
